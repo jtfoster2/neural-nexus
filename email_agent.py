b63c0fa8dc397fc90cce5143770d4b4e1708a7c4
Catherin2 <cayeronwi@gmail.com>
2025-09-06 09:52:21 +0000

    git commit -S -m "email_agent commit"

--- email_agent.py
+++ email_agent.py
@@ -3,3 +3,7 @@ from typing import TypedDict, Annotated, List
 from langgraph.graph import StateGraph, END
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+
+
+# import the tool
+from sendgrid_tool import send_order_email
