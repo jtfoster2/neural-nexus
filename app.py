823635f375d989e79db032b884dea2c59ef54e80
Catherin2 <cayeronwi@gmail.com>
2025-09-18 01:59:51 +0100

    git commit -m "code update: app.py"

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
-from agent import app, ask_agent #imported ask_agent function to prevent AttributeError
+# imported ask_agent function to prevent AttributeError
+from agent import app, ask_agent
 
 st.set_page_config(page_title="Customer Support Agent")
 
 # --- Title ---
-st.markdown("<h1 style='text-align: center;'>Customer Support Agent</h1>", unsafe_allow_html=True) #Centered title
+st.markdown("<h1 style='text-align: center;'>Customer Support Agent</h1>",
+            unsafe_allow_html=True)  # Centered title
 
 # --- Session state for chat history ---
 if "messages" not in st.session_state:
-    st.session_state.messages = []  #Stores chat history in structure {"role": "user"/"assistant", "content": str} 
+    # Stores chat history in structure {"role": "user"/"assistant", "content": str}
+    st.session_state.messages = []
 
 # --- Display chat history ---
 for msg in st.session_state.messages:
@@ -24,6 +27,8 @@ if prompt := st.chat_input("Ask me anything about your order, billing, etc."):
     st.chat_message("user").write(prompt)
 
     with st.spinner("Thinking..."):
-        reply = ask_agent(prompt, thread_id="customer-support") #######thread_id will need to updated dynamically in future versions#######
-        st.session_state.messages.append({"role": "assistant", "content": reply})
+        # thread_id will need to updated dynamically in future versions#######
+        reply = ask_agent(prompt, thread_id="customer-support")
+        st.session_state.messages.append(
+            {"role": "assistant", "content": reply})
         st.chat_message("assistant").write(reply)
